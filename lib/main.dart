bd1280d7717bfa08b993bb8c1934f3bbfc84207b
Davi Negreiros <davi.negreiiros36@gmail.com>
2025-01-07 09:28:51 -0300

    aula 03

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +11,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    http.get(Uri.https('google.com')).then((response) {
+      if (kDebugMode) {
+        print(response.body);
+      }
+    });
+
     return const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
         body: Center(
-          child: Text('AULA 01'),
+          child: Text('AULA 03'),
         ),
       ),
     );
